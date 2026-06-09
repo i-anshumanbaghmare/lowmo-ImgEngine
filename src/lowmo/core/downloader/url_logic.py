9079d3c7ffedf51c3683b8ae9bbf363cc25f5d7a
Anshuman Baghmare <i.anshuman.baghmare@gmail.com>
2026-06-09 20:05:55 +0530

    delete old format files

--- src/lowmo/core/downloader/url_logic.py
+++
@@ -1,4 +0,0 @@
-# DEPRECATED: This file has been renamed to downloader_utils.py.
-# Please import from src.lowmo.core.downloader.downloader_utils instead.
-# (This file is kept temporarily to avoid import breaks while refactoring)
-from .downloader_utils import sanitize_url, detect_source, reconstruct_hf_url
